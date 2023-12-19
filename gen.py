88ba5410fff480d688aa0cebbfc473e71d486312
Evan Zhou <evanzhou413@gmail.com>
2023-12-18 16:45:34 -0800

    add comments

--- gen.py
+++ gen.py
@@ -3,22 +3,26 @@ import sys
 import validators
 
 def gen_cart_link():
+    # open file
     file = open(sys.argv[1], newline='')
     data = list(csv.reader(file, delimiter=','))
     file.close()
         
-    first_line = data.pop(0)[0]
+    # get shop url
+    shop_url = data.pop(0)[0]
     
-    if not validators.url(first_line):
+    # check if valid url
+    if not validators.url(shop_url):
         return
     
-    url = first_line + "cart/"
+    url = shop_url + "cart/"
     items = []
             
+    # append items to link
     for row in data:
-        if len(row) == 1:
+        if len(row) == 1: # case if no quantity is defined
             items.append(row[0] + ":1")
-        elif len(row) == 2:
+        elif len(row) == 2: #case if a quantity is defined
             items.append(row[0] + ":" + row[1])
         
     return url + ",".join(items) + "?payment=shop_pay"
